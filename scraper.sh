711c74b2a9d6b78b41921792113e827e5f79b72b
Bruno Ivančić <52743786+U6487954399@users.noreply.github.com>
2021-03-25 18:40:30 +0100

    Create scraper.sh

---
+++ scraper.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+#	usage:
+#	./scraper.sh -w "website"
+
+# defaults
+website="app.pickle.finance"
+
+OPTIND=1
+# Reset in case getopts has been used previously in the shell
+
+while getopts ":w:" arg; 
+do 
+	case "$arg" in 
+	w) website="$OPTARG"
+	;;
+	esac
+done
+
+echo "downloading website: $website"
+
+wget  -r -pk $website
+
+
+grep -RwoE '0x.{40}' $website | cut -d ":" -f2 | grep -v '["()$"%&= ,]'| awk '!seen[$0]++' > $website.txt
+
+rm -r $website
+
+
+exit 0
